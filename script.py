174798af4dc16881a0c563fa473549cb8da541d1
Sreeharsha Potu <harshapotu@gmail.com>
2019-04-24 21:35:00 -0400

    strip newline characters

--- script.py
+++ script.py
@@ -8,12 +8,12 @@ actualFile = "output.iq"
 fileToWriteTo = open(actualFile, 'wb')
 with open('test.txt') as fp:
   for line in fp:
-    args = line.split(" ")
-    message = subprocess.check_output(["python", "generate_message.py"] + args[0:-1])
-    message = message.rstrip()
-    print("Message: " + message)
-    ignore = subprocess.check_output(["python3", "encode_wavebird.py", message, fileName, args[-1].rstrip()])
-    print("Ignore: " + ignore)
+    args = line.rstrip().split(" ")
+    print("args: ", args)
+    message = subprocess.check_output(["python", "generate_message.py"] + args[0:-1]).rstrip()
+    print("Message: " + str(message))
+    ignore = subprocess.check_output(["python3", "encode_wavebird.py", message, fileName, args[-1]])
+    print("Ignore: " + str(ignore))
     with open(fileName, 'rb') as tempFile:
       fileToWriteTo.write(tempFile.read())
     os.remove(fileName)
